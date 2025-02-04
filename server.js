9a7788f49109841929542540126266fdce36b346
CoVanAn <covanan632003@gmail.com>
2025-02-04 15:59:31 +0700

    v1.31

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoute.js';
-import userRouter from './routes/userRoute.js';
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
-import categoryRouter from './routes/categoryRoute.js';
+// import foodRouter from './routes/foodRoute.js';
+// import userRouter from './routes/userRoute.js';
+// import cartRouter from './routes/cartRoute.js';
+// import orderRouter from './routes/orderRoute.js';
+// import categoryRouter from './routes/categoryRoute.js';
 import 'dotenv/config'
 
 
@@ -13,27 +13,27 @@ import 'dotenv/config'
 const app = express();
 const port = process.env.PORT || 4003;
 
-// middleware
-app.use(express.json());
-app.use(cors());
+// // middleware
+// app.use(express.json());
+// app.use(cors());
 
-// db config
-connectDB();
+// // db config
+// connectDB();
 
-// api endpoints
-app.use('/api/food', foodRouter);
-app.use('/images', express.static('uploads'));
-app.use('/api/user', userRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
-app.use('/api/category',categoryRouter);
+// // api endpoints
+// app.use('/api/food', foodRouter);
+// app.use('/images', express.static('uploads'));
+// app.use('/api/user', userRouter);
+// app.use('/api/cart', cartRouter);
+// app.use('/api/order', orderRouter);
+// app.use('/api/category',categoryRouter);
 
-// api routes
+// // api routes
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
 });
 
-// listen
+// // listen
 app.listen(port, () => {
     console.log(`Server is running on port ${port} ex: http://localhost:${port}`);
 });
